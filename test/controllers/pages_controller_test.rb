f362edc6a41b8a7666b0b060b5c6dfc670745a20
surrus <xsurrus@gmail.com>
2015-12-14 04:40:32 -0700

    dynamic titles

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,16 +4,19 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "home"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "about"
   end
 
   test "should get directions" do
     get :directions
     assert_response :success
+    assert_select "title", "directions"
   end
 
 end
